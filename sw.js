75bebc9e69981ca6a08ed196b999cff4db4bd3d9
CroakBot1 <apploverss1@gmail.com>
2025-12-28 12:25:10 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,19 @@
-const CACHE_NAME = "quantum-pwa-v1";
+const CACHE_NAME = "private-chat-pwa-v1";
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
         "./",
         "./index.html",
         "./manifest.json"
-      ]);
-    })
+      ])
+    )
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
